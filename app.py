de426167a2ed8e8b32d9a53b0f0742839a6b2214
nurainundaulayy <nurainundaulay@outlook.co.id>
2024-11-12 16:33:30 +0700

    app.py

--- app.py
+++ app.py
@@ -19,8 +19,43 @@ dashboard_page = st.Page(
     icon=":material/team_dashboard:"
 )
 
+informasi_page = st.Page(
+    page="views/informasi.py",
+    title="Infromasi",
+    icon=":material/info:"
+)
+
 # --- NAVIGATION SETUP ---
-pg = st.navigation(pages=[home_page, chatbot_page, dashboard_page], expanded=False)
+pg = st.navigation(pages=[home_page, informasi_page, chatbot_page, dashboard_page], expanded=False)
 
 # --- RUN NAVIGATION ---
 pg.run()
+
+
+
+# import streamlit as st
+
+
+# # --- PAGE SETUP ---
+# home_page = st.Page(
+#     page="views/home.py",
+#     title="Home",
+#     icon=":material/home:",
+#     default=True,
+# )
+# chatbot_page = st.Page(
+#     page="views/chatbot.py",
+#     title="Chatbot Assistant",
+#     icon=":material/smart_toy:"
+# )
+# dashboard_page = st.Page(
+#     page="views/dashboard.py",
+#     title="Dashboard Monitoring",
+#     icon=":material/team_dashboard:"
+# )
+
+# # --- NAVIGATION SETUP ---
+# pg = st.navigation(pages=[home_page, chatbot_page, dashboard_page], expanded=False)
+
+# # --- RUN NAVIGATION ---
+# pg.run()
